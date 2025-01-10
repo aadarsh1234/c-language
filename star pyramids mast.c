6ac07b6128582ba6447e6d7c3cd7598b16b92b92
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-01-09 18:04:03 -0800

    Update star pyramids mast.c

--- star pyramids mast.c
+++ star pyramids mast.c
@@ -73,3 +73,45 @@ int main()
 
     return 0;
 }
+ABCDEFG
+ABC EFG
+AB.  FG 
+A.    G
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Hello World");
+    scanf("%d",&n);
+    int nst=n;
+    int nsp=1;
+    for(int i=1;i<=2*n+1;i++){
+        char ch=(char)(i+64);
+        printf("%c",ch);
+    }
+    printf("\n");
+    for(int i=1;i<=n;i++){
+        int a=1;
+        for(int j=1;j<=nst;j++){
+            char ch=(char)(j+64);
+            printf("%c",ch);
+            a++;
+        }
+        for(int k=1;k<=nsp;k++){
+            printf(" ");
+            a++;
+        }
+       for(int j=1;j<=nst;j++){
+           char ch=(char)(j+64);
+            printf("%c",ch);
+            a++;
+        }
+        nsp+=2;
+        nst--;
+        printf("\n");
+       
+    }
+
+    return 0;
+}
