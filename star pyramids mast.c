470212b71e20f7f2f278ff484641696a1c141fd0
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-01-09 18:06:48 -0800

    Update star pyramids mast.c

--- star pyramids mast.c
+++ star pyramids mast.c
@@ -94,7 +94,7 @@ int main()
     for(int i=1;i<=n;i++){
         int a=1;
         for(int j=1;j<=nst;j++){
-            char ch=(char)(j+64);
+            char ch=(char)(a+64);
             printf("%c",ch);
             a++;
         }
@@ -103,7 +103,7 @@ int main()
             a++;
         }
        for(int j=1;j<=nst;j++){
-           char ch=(char)(j+64);
+           char ch=(char)(a+64);
             printf("%c",ch);
             a++;
         }
