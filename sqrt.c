16ef1008f765df5ff7154b5a224dd37fa0cba7e7
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-01-11 00:13:21 -0800

    Update

---
+++ sqrt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<math.h>
+int main(){
+    int a;
+    printf("enter the value of a and b");
+    scanf("%d",&a);
+    int root=sqrt(a);
+    printf("the sqrt is %d", root);
+
+return 0;
+}
