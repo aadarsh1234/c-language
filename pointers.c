3a13462191d00ae638550fe468ee4c660c4fd8dd
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-01-12 04:40:51 -0800

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -8,3 +8,25 @@ int main()
     printf("%p",x);
     return 0;
 }
+(2)
+void  swap(int *x,int *y){
+   int temp;
+   temp=*x;
+   *x=*y;
+   *y=temp;
+   return;
+}
+int main()
+{
+  int a;
+  printf("enter value a " );
+  scanf("%d",&a);
+  int b;
+  printf("enter value b");
+  scanf("%d",&b);
+  swap(&a,&b);
+  printf("the vakue of a is %d\n",a);
+  printf("the value of b is %d\n",b);
+  
+    return 0;
+}
