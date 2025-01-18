a648eb21ec5eb61157b6214c623fa8770db3e7a6
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-01-18 12:53:23 -0800

    Update recursion.c

--- recursion.c
+++ recursion.c
@@ -35,3 +35,22 @@
         return 0;
         
     }
+
+// print sum from 1 to n (parameterised) 4=10
+#include <stdio.h>
+void sum(int n,int s){
+    if(n==0){
+        printf("%d",s);
+        return;
+    }
+    sum(n-1,s+n);
+    return;
+}
+int main (){
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    sum(n,0);
+    return 0;
+}
+
