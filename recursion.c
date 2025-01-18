91be553258dfa835f9ccb5eec6dc27d7ee2c5241
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-01-18 12:21:05 -0800

    Update recursion.c

--- recursion.c
+++ recursion.c
@@ -1,17 +1,37 @@
-// printf 1 to n.......
+// printf 1 to n....... increasing /deacrsing (D 
 
 #include<stdio.h>
-    void increasing(int x,int n){
-       if(x>n)return;
-        printf("%d\n",x);
-       increasing(x+1,n);
+    void increasing(int n){
+       if(n==0)return;
+        printf("%d\n",n);
+       increasing(n-1);
         return;
     }
     int main(){
         int n;
         printf("enter the number");
         scanf("%d",&n);
-        increasing(1,5);
+        increasing(n);
+        return 0;
+        
+    }
+// printf 1 to n.......321123
+
+#include<stdio.h>
+    void increasing(int n){
+       if(n==0)return;
+        printf("%d\n",n);
+        increasing(n-1);
+        printf("%d\n",n);
+    
+       
+        return;
+    }
+    int main(){
+        int n;
+        printf("enter the number");
+        scanf("%d",&n);
+        increasing(n);
         return 0;
         
     }
