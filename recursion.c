47406dca7502b57b7e4f227d9a2bde9923b1d463
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-01-19 02:49:12 -0800

    Update recursion.c

--- recursion.c
+++ recursion.c
@@ -71,4 +71,19 @@ int main()
     printf("the sum is %d",add);
     return 0;
 }
+ // this raised to the power this 
+    #include<stdio.h>
+    int power(int a,int b){
+    if(b==0)return 1;
+    return a*power(a,b-1);
+    }
+    int main(){
+        int a,b;
+        printf("enter a and b:");
+        scanf("%d %d",&a,&b);
+        int p=power(a,b);
+        printf("%d is raised to power of %d is %d",a,b,p);
+    
+    return 0;
+    }
 
