41166c3d8268cb1e3fd15f43006e346c9c786461
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-01-19 18:12:00 +0530

    Update recursion.c

--- recursion.c
+++ recursion.c
@@ -106,3 +106,21 @@ int main()
 
     return 0;
 }
+
+// stair path 
+
+#include <stdio.h>
+int stair(int n){
+    if(n<=3) return n;
+    return stair(n-1)+stair(n-2)+stair(n-3);
+}
+int main()
+{
+    int n;
+    printf("enter n:");
+    scanf("%d",&n);
+    int way=stair(n);
+    printf("%d",way);
+
+    return 0;
+}
