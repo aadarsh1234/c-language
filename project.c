80a78d9da489523a4fd666337e9205442150a2dd
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-01-26 22:00:57 +0530

    Update project.c

--- project.c
+++ project.c
@@ -1,3 +1,4 @@
+ (arithmetic operator)
 #include <stdio.h>
 
 int main()
@@ -24,4 +25,43 @@ int main()
   printf("Module   = %d\n",mod);
   
     return 0;
+}
+
+(radius of circle)
+
+#include <stdio.h>
+int main(){
+    float radius, area;
+    // user enter the radius 
+    printf("enter radius of the circle:");
+    scanf("%f",&radius);
+    
+    // calculate radius of circle 
+    area= 3.14159*radius*radius;
+    
+    // print the result 
+    printf("Area of circle in floating point notation = %.2f\n",area);
+    printf("Area of circle in exponential notation = %.2e\n",area);
+    
+    return 0;
+}
+
+(logical operator)
+
+#include<stdio.h>
+int main (){
+int a=5,b=10,c=0;
+
+// Logical AND (&&) operator 
+if (a>0 && b>0){
+printf("both a and b are positive number.\n");
+}
+// logical OR (||) operator 
+if(a>0 || c>0){
+printf("atleast one a or c is a positive number. \n");
+}
+// Logical NOT(!) operator 
+if(!c)
+printf("c is equal to 0.");
+return 0;
 }
