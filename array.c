e4377494cc8d1eb872cb78d6b6737f40d26ddf25
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-02-24 22:58:12 +0530

    Update array.c

--- array.c
+++ array.c
@@ -365,3 +365,24 @@ int main()
    }
     return 0;
 }
+unique number
+#include <stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+   int arr[7]={1,2,3,6,5,6,7};
+   for(int i=0;i<=6;i++){
+       bool flag=false;
+      for(int j=i+1;j<=6;j++){
+          if(arr[i]==arr[j]){
+              flag=true;
+          }
+      }
+      if(flag==false){
+          printf("%d",arr[i]);
+          break;
+      }
+   }
+    return 0;
+}
