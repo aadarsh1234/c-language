48a388496b859833b2efb6d7027b187706aa3277
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-02-25 08:36:56 +0530

    Update 2D array.c

--- 2D array.c
+++ 2D array.c
@@ -190,3 +190,27 @@ int idx=-1;
     printf("%d is the maximum value and its idx%d",max,idx);
     return 0;
 }
+(9) find max zero one in index 
+{1,0,1,1},{1,0,0,1},{1,1,1,1}
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[3][4]= {{1,0,0,1},{0,1,0,1},{1,1,1,1}};
+    int maxcount=0;
+    int maxidx=0;
+    for(int i=0;i<3;i++){
+        int count=0;
+        for(int j=0;j<4;j++){
+            if(arr[i][j]==1) count++;
+        }
+        if(maxcount<count){
+            maxcount=count;
+            maxidx=i;
+        }
+        printf("\n");
+    }
+    printf("%d",maxidx);
+
+ 
