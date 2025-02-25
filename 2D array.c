9decbb75fd40fcf17e33633d4e000590000cd0c7
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-02-25 19:02:26 +0530

    Update 2D array.c

--- 2D array.c
+++ 2D array.c
@@ -309,5 +309,58 @@ int main()
     printf("\n");
     for(int i=0;i<n;i++){
   
+(12) multiply of matrix 
+#include <stdio.h>
 
+int main()
+{
+    int m;
+    printf("enter row of 1st matrix:");
+    scanf("%d",&m);
+    int n;
+    printf("enter column of 1st matrix:");
+    scanf("%d",&n);
+    int a[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int p;
+    printf("enter row of 2nd matrix:");
+    scanf("%d",&p);
+    int q;
+    printf("enter column of 2nd matrix:");
+    scanf("%d",&q);
+    int b[p][q];
+    for(int i=0;i<p;i++){
+        for(int j=0;j<q;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    if(n!=p){
+        printf("the matrix cannot multipy");
+    }
+    else{
+      int res[m][q];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<q;j++){
+                res[i][j]=0;
+                for(int k=0;k<n;k++){
+                    res[i][j]+=a[i][k]*b[k][j];
+                }
+           }
+    }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<q;j++){
+            printf("%d",res[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
+    
+
+    return 0;
+}
  
