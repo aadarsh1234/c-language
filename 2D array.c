77e12b0da28d100a03802891f3ab66b30c81113a
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-02-25 16:29:21 +0530

    Update 2D array.c

--- 2D array.c
+++ 2D array.c
@@ -249,4 +249,37 @@ int main()
     return 0;
 }
 
+(11) swap of transpose
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the row/column");
+    scanf("%d",&n);
+    printf("enter all element:");
+    int arr[n][n];
+    for(int i=0;i<n;i++){
+     for(int j=0;j<n;j++){
+         scanf("%d",&arr[i][j]);
+     }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=1;j<n;j++){
+            int temp=arr[i][j];
+            arr[i][j]=arr[j][i];
+            arr[j][i]=temp;
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
  
