93f5a67cd54863acc982f7f40d735bab96128413
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-02-25 17:03:08 +0530

    Update 2D array.c

--- 2D array.c
+++ 2D array.c
@@ -282,4 +282,32 @@ int main()
     return 0;
 }
 
+(12) rotate a transpose of matrix
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter row/column:");
+    scanf("%d",&n);
+    printf("enter all elment:");
+    int arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    // transpose ofa matrix
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            int temp= arr[i][j];
+            arr[i][j]=arr[j][i];
+            arr[j][i]=temp;
+        }
+    }
+    printf("\n");
+    for(int i=0;i<n;i++){
+  
+
  
