5962031edc95bca16f21a51f53357425f92b69b1
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-02-25 09:51:52 +0530

    Update 2D array.c

--- 2D array.c
+++ 2D array.c
@@ -213,4 +213,40 @@ int main()
     }
     printf("%d",maxidx);
 
+(10) transpose of matrix
+
+#include <stdio.h>
+
+int main()
+{
+    int r;
+    printf("enter row:");
+    scanf("%d",&r);
+    int c;
+    printf("enter column:");
+    scanf("%d",&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("\n");
+    int brr[c][r];
+    for(int i=0;i<c;i++){
+        for(int j=0;j<r;j++){
+            brr[i][j]= arr[j][i];
+        }
+        printf("\n");
+    }
+    for(int i=0;i<c;i++){
+        for(int j=0;j<r;j++){
+            printf("%d",brr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
  
