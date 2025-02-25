dfca7546e24a31b7f09bfc0013dfa7a1e080e363
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-02-25 06:28:31 +0530

    Update 2D array.c

--- 2D array.c
+++ 2D array.c
@@ -71,3 +71,32 @@ int main()
 
     return 0;
 }
+(5) marks or roll no 4 student 
+#include <stdio.h>
+
+int main()
+{ 
+    int r;
+    printf("enter nuber of row:");
+    scanf("%d",&r);
+    int c;
+    printf("enter number of column:");
+    scanf("%d",&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+     for(int j=0;j<c;j++){
+         scanf("%d",&arr[i][j]);
+     }
+    }
+    printf("\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+   
+   
+
+    return 0;
+}
