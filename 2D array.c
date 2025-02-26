c9910b9a8f6dea56e75b4904686d1b34a2bcdeef
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-02-26 05:47:45 +0530

    Update 2D array.c

--- 2D array.c
+++ 2D array.c
@@ -400,4 +400,39 @@ int main()
 
     return 0;
 }
- 
+ (14) wave form-> 2 column bise 
+#include <stdio.h>
+
+int main()
+{
+    int m;
+    printf("enter row of matrix:");
+    scanf("%d",&m);
+    int n;
+    printf("enter the column:");
+    scanf("%d",&n);
+    printf("enter element of matrix:");
+    int arr[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("\n");
+    // wave form
+    for(int j=0;j<n;j++){
+        if(j%2==0){
+            for(int i=0;i<m;i++){
+                printf("%d ",arr[i][j]);
+            }
+        }
+        else{
+            for(int i=m-1;i>=0;i--){
+                printf("%d ",arr[i][j]);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
