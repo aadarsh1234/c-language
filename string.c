71ee5a7edfbe54424ed297e74187ac7c623b918d
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-02-27 05:19:41 +0530

    Update string.c

--- string.c
+++ string.c
@@ -46,3 +46,27 @@ int main()
     printf("%d",size);
     return 0;
 }
+(4) reverse of string
+#include <stdio.h>
+
+int main()
+{
+    char str[40];
+    printf("enter string");
+    scanf("%[^\n]s",str);
+    int k=0;
+    int  size=0;
+    while(str[k]!='\0'){
+    size++;
+    k++;
+    }
+    for(int i=0,j=size-1;i<=j;i++,j--){
+        char temp=str[i];
+        str[i]=str[j];
+        str[j]=temp;
+    }
+    puts("the reverse is");
+    puts(str);
+    
+     return 0;
+}
