225339897b7e76f9d60bcb21d5f000ac87d326c5
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-02-28 16:41:47 +0530

    Update string.c

--- string.c
+++ string.c
@@ -70,3 +70,18 @@ int main()
     
      return 0;
 }
+(5) charcter array pointer
+#include <stdio.h>
+
+int main()
+{
+    char str[]="college wallah";
+    char* ptr=str;
+    int i=0;
+    while(*ptr!='\0'){
+        printf("%c",*ptr);
+        ptr++;
+        i++;
+    }
+    return 0;
+}
