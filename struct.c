4d9217d699dc7c920bfb84400e744e0db2b986f5
aadarsh1234 <dubeyaadarsh637@gmail.com>
2025-03-02 15:45:24 +0530

    Update struct.c

--- struct.c
+++ struct.c
@@ -118,3 +118,46 @@ int main()
 
     return 0;
 }
+
+(5) array of structure 
+
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    typedef  struct pokemon {
+        char name[15];
+        int hp;
+        int speed;
+        int attack;
+        char tier;
+    } pokemon;
+     pokemon arr[3];
+     arr[0].hp=56;
+     arr[0].speed=78;
+     arr[0].attack=70;
+     arr[0].tier='A';
+     strcpy(arr[0].name,"charizard");
+     
+     arr[1].hp=46;
+     arr[1].speed=68;
+     arr[1].attack=90;
+     arr[1].tier='B';
+     strcpy(arr[1].name,"pikachu");
+     
+     arr[2].hp=46;
+     arr[2].speed=68;
+     arr[2].attack=90;
+     arr[2].tier='B';
+     strcpy(arr[2].name,"Mewtwo");
+     
+     for(int i=0;i<3;i++){
+         printf("%s\n",arr[i].name);
+         printf("%d\n",arr[i].speed);
+         printf("%d\n",arr[i].hp);
+         printf("%c\n",arr[i].tier);
+         printf("%d\n",arr[i].attack);
+     }
+    return 0;
+}
